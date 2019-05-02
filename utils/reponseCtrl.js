548e1751a17f752e8e93c88cd81e3631c99629c0
Edwin <edwin@flowerpotmobilty.com>
2019-05-02 20:26:54 +0530

    added router, dbOperation and utils file

---
+++ utils/reponseCtrl.js
@@ -0,0 +1,42 @@
+var HttpStatus=require('http-status');
+
+exports.sendSuccess=function (res,data){
+    res.status(HttpStatus.OK).json({
+        status:'success',
+        code: HttpStatus.OK,
+        data:data
+    });
+    return;
+}
+exports.sendInternalServerError=function (res,data){
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status:'failure',
+        code: HttpStatus.INTERNAL_SERVER_ERROR,
+        error:data
+    });
+    return;
+}
+exports.sendNotFound=function (res,data){
+    res.status(HttpStatus.NOT_FOUND).json({
+        status:'failure',
+        code: HttpStatus.NOT_FOUND,
+        error:data
+    });
+    return;
+}
+exports.sendBadRequest=function (res,data){
+    res.status(HttpStatus.BAD_REQUEST).json({
+        status:'failure',
+        code: HttpStatus.BAD_REQUEST,
+        error:data
+    });
+    return;
+}
+exports.sendUnAuthorized=function (res,data="UNAUTHORIZED USER"){
+    res.status(HttpStatus.UNAUTHORIZED).json({
+        status:'failure',
+        code: HttpStatus.UNAUTHORIZED,
+        error: data
+    });
+    return;
+}
