9337d81d8195121bdc8a1ef16b6e06cac113a863
Edwin <edwin@flowerpotmobilty.com>
2019-05-02 20:26:33 +0530

    added router and dbOperation

---
+++ routes/contactRoutes.js
@@ -0,0 +1,7 @@
+let express = require('express')
+let router = express.Router()
+let ContactController = require('../server/controllers/ContactsController')
+
+// router.post("contacts", ContactController)
+
+module.exports = router
