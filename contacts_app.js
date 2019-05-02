4a1300b0c83fdaed0f3be60ca2b9fab3f353b00b
Edwin <edwin@flowerpotmobilty.com>
2019-05-02 18:47:53 +0530

    added contact and contactGroup schema and controller

--- contacts_app.js
+++ contacts_app.js
@@ -6,6 +6,7 @@ let SERVER_CONFIG = require("./config/serverConfig")
 let port = process.env.PORT || SERVER_CONFIG.PORT;
 let cors = require('cors');
 let dbConnection = require('./config/dbConnection')
+let router = require('./router')
 
 dbConnection.connect() //trigger database connection
 
@@ -18,6 +19,7 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 
+router(app);
 app.listen(port);
 console.log('App is listening on port: ' + port);
 console.log('http://localhost:' + port);
